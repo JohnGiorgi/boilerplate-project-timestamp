4aea1d6d3c67c6c44e8d43a4f0b338d7f3a6a3d5
johngiorgi <johnmgiorgi@gmail.com>
2018-11-17 11:32:21 -0500

    :sparkles: Submission to the Timestep Microservice prject on FCC

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -18,15 +18,24 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// all challange code goes here
+app.get("/api/timestamp/:date_string", (req, res) => {
+  const date_string = req.params.date_string;
+  // try converting to int to catch unix timestamps
+  let d = new Date(date_string);
+  if (!isValidDate(d)) {
+    d = new Date(parseInt(date_string));
+  }
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({"unix": d.getTime(), "utc" : d.toUTCString()});
 });
 
-
+// help functions
+function isValidDate(d) {
+  return d instanceof Date && !isNaN(d);
+}
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
